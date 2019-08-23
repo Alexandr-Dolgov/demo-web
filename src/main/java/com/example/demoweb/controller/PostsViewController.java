60805f2da352f1f3448cce718dd769be003ebd4b
v.polyakov <vladimir.p.polyakov@gmail.com>
2019-08-23 18:35:49 +0500

    post

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -4,10 +4,10 @@ import com.example.demoweb.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
@@ -22,15 +22,23 @@ public class PostsViewController {
         return "list";
     }
 
-    @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
-    public String single(@PathVariable("id") Integer id) {
-        return "Здесь будет страница с картинкой " + id;
+    public String single(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("list",
+                postsService.listAllPosts().subList(id, id + 1));
+        return "list";
     }
 
     @RequestMapping(path = "/new", method = RequestMethod.GET)
     public String create() {
         return "create";
     }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        Long id = postsService.create(text);
+        return "redirect:/post/" + id;
+    }
+
 
 }
--- src/main/java/com/example/demoweb/service/PostsService.java
+++ src/main/java/com/example/demoweb/service/PostsService.java
@@ -9,14 +9,21 @@ import java.util.List;
 @Service
 public class PostsService {
 
-    public List<Post> listAllPosts() {
-        List<Post> posts = new ArrayList<>();
+    List<Post> posts = new ArrayList<>();
 
+    {
         posts.add(new Post(0L, "Первый пост в списке", 0));
         posts.add(new Post(1L, "Второй пост в списке", 0));
         posts.add(new Post(2L, "Третий пост в списке", 0));
+    }
 
+    public List<Post> listAllPosts() {
         return posts;
     }
 
+    public Long create(String text) {
+        long id = posts.size();
+        posts.add(new Post(id, text, 0));
+        return id;
+    }
 }
