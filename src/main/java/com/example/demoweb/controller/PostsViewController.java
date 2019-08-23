17396530ac73d4a5e84d991c59548a0b0c6311f7
v.polyakov <vladimir.p.polyakov@gmail.com>
2019-08-23 18:46:35 +0500

    рефакторинг

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -4,7 +4,6 @@ import com.example.demoweb.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,21 +23,10 @@ public class PostsViewController {
 
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("appName", "Look at this!");
         model.addAttribute("list",
                 postsService.listAllPosts().subList(id, id + 1));
         return "list";
     }
-
-    @RequestMapping(path = "/new", method = RequestMethod.GET)
-    public String create() {
-        return "create";
-    }
-
-    @RequestMapping(path = "/new", method = RequestMethod.POST)
-    public String doCreate(@ModelAttribute("text") String text) {
-        Long id = postsService.create(text);
-        return "redirect:/post/" + id;
-    }
-
 
 }
